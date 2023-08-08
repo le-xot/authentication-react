239b75a1a628e4142d9d4ed885203cb6f58b2db6
le-xot <lexot.dev@gmail.com>
2023-08-09 02:42:10 +0500

    sum

--- src/components/bodies/loginBody.tsx
+++ src/components/bodies/loginBody.tsx
@@ -15,11 +15,11 @@ export const LoginBody: React.FC = () => {
     <Box h={`calc(100vh - 80px)`} bg={"#141517"}>
       <Flex justifyContent={"center"} alignItems={"center"} h={"100%"}>
         <SimpleGrid>
-          <FormControl w={"500px"} paddingBottom={"20px"} isRequired>
+          <FormControl w={"500px"} paddingBottom={"20px"}>
             <FormLabel>Username</FormLabel>
             <Input type="text" placeholder="Your username" />
           </FormControl>
-          <FormControl paddingBottom={"20px"} isRequired>
+          <FormControl paddingBottom={"20px"}>
             <FormLabel>Password</FormLabel>
             <Input type="password" placeholder="Your password" />
           </FormControl>
